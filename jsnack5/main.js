a6103effb608934d6744aaaddd02b0636fd5113a
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-23 15:29:41 +0200

    finito snack 5

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,2 +1,13 @@
 // JSnack 5: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
-// JSnack 6: Stampa le potenze di 2 fino a 1000
+
+
+// chiedi all'utente un numero casuale n
+var N = parseInt(prompt('Scrivi un numero a caso'));
+
+// crea un ciclo for di N ripetizioni
+for (var i = 0; i < N; i++) {
+    // eleva al cubo le i
+    var cubo = Math.pow((i + 1), 3);
+    // stampa la lista di numeri al cubo
+    console.log(cubo);
+}
