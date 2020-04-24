8b63d9571933fc8eefa618695e47d921d32b6905
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-24 10:19:27 +0200

    appunti + metodi alternativi

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -5,7 +5,7 @@
 var N = parseInt(prompt('Scrivi un numero a caso'));
 
 // crea un ciclo for di N ripetizioni
-for (var i = 1; i <= N; i++) {
+for (var i = 0; i <= N; i++) {
     // eleva al cubo le i
     var cubo = Math.pow(i, 3);
     // stampa la lista di numeri al cubo
--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,8 +1,28 @@
 // JSnack 6: Stampa le potenze di 2 fino a 1000
-var base = 2
+var base = 2;
 // crea un ciclo for per gli esponenti
 for (var esponente = 0; Math.pow(base, esponente) < 1000; esponente++) {
 
     var potenza = Math.pow(base, esponente);
     console.log(potenza);
 }
+
+// // metodo alternativo
+// // JSnack 6: Stampa le potenze di 2 fino a 1000
+// var base = 2;
+// var potenza = 0;
+// // crea un ciclo for per gli esponenti
+// for (var esponente = 0; potenza < 1000; esponente++) {
+//     console.log(potenza);
+// questo va sopra perchè è vincolante l'ordine di ripetizione del ciclo.
+//     var potenza = Math.pow(base, esponente);
+//
+// }
+
+// dichiarazione variabile
+// espressioneIniziale, se esiste, viene eseguita. L'espressione di solito inizializza uno o più indici, ma la sintassi permette di scrivere espressioni con diversi gradi di compessità. Questa espressione può anche dichiarare delle variabili.
+// test
+// La condizione viene verificata. Se il suo valore è true, l'espressione istruzione viene eseguita. Se invece condizione è false, il ciclo finisce. Se condizione è omessa, l'espressione si assume sia true.
+// istruzione viene esguita. Per eseguire diverse istruzioni, è necessario usare il blocco ({ ... }) per raggrupparle.
+// incremento
+// Viene incrementata la l'espressione incremento, se esiste, eseguita, e il ciclo for va al passo successivo.
